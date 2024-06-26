50912ea1f6edd82b55c0c97efbf5c90aa3f6975b
Alekseev Konstantin <alexcseev1990@gmail.com>
2024-06-26 22:47:12 +0300

    fix

---
+++ src/components/ActivityItem.vue
@@ -0,0 +1,44 @@
+<template>
+     <li class="flex flex-col gap-2 p-4">
+        <div class="flex items-center gap-2">
+            <BaseButton>
+                <TrashIcon class="h-8"/>
+            </BaseButton>
+            <span class="truncate text-xl">{{ activity }}</span>
+        </div>
+        <div>
+            <BaseSelect :selected="secondsToComplite" class="font-mono" placeholder="h:mm" :options="periodSelectOptions" />
+        </div>
+    </li> 
+</template>
+
+<script setup>
+import { ref } from "vue";
+import { TrashIcon } from "@heroicons/vue/24/solid";
+import BaseButton from "./BaseButton.vue";
+import BaseSelect from "./BaseSelect.vue";
+
+defineProps({
+    activity: {
+        required: true,
+        type: String,
+    },
+})
+
+const secondsToComplite = ref(15)
+
+const periodSelectOptions = [
+    {
+        value: 15,
+        label: '0:15'
+    },
+    {
+        value: 30,
+        label: '0:30'
+    },
+    {
+        value: 45,
+        label: '0:45'
+    },
+]
+</script>
--- src/pages/TheActivities.vue
+++ src/pages/TheActivities.vue
@@ -1,3 +1,15 @@
 <template>
-    <h1>Activities</h1>
+    <div>
+        <ul class="divide-y">
+            <ActivityItem v-for="activity in activities" :activity="activity" :key="activity" />
+        </ul>
+    </div>
 </template>
+
+<script setup>
+import ActivityItem from "../components/ActivityItem.vue"
+
+const activities = ['Codding', 'Reading', 'Training'];
+
+
+</script>
